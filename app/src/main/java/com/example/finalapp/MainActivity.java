f672719ceccbda848fba824c660627f2c3c97d19
Никита Копылов <nikitakopylov@MacBook-Pro-Nikita.local>
2023-03-27 19:12:24 +0500

    inial commit

---
+++ app/src/main/java/com/example/finalapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.finalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    public static boolean isLeftPressed = false; // нажата левая кнопка
+    public static boolean isRightPressed = false; // нажата правая кнопка
+    public static boolean isMiddlePressed = false; // нажата средняя кнопка
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // set the content view
+        GameView gameView = new GameView(this); // создаём gameView
+
+        LinearLayout gameLayout = findViewById(R.id.gameLayout); // находим gameLayout
+        gameLayout.addView(gameView); // и добавляем в него gameView
+
+
+
+        Button leftButton = findViewById(R.id.leftButton); // находим кнопки
+        Button rightButton = findViewById(R.id.rightButton);
+
+        leftButton.setOnTouchListener(this); // и добавляем этот класс как слушателя (при нажатии сработает onTouch)
+        rightButton.setOnTouchListener(this);
+
+    }
+
+
+
+    public boolean onTouch(View button, MotionEvent motion) {
+        switch(button.getId()) { // определяем какая кнопка
+            case R.id.leftButton:
+                switch (motion.getAction()) { // определяем нажата или отпущена
+                    case MotionEvent.ACTION_DOWN:
+                        isLeftPressed = true;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        isLeftPressed = false;
+                        break;
+                }
+                break;
+            case R.id.rightButton:
+                switch (motion.getAction()) { // определяем нажата или отпущена
+                    case MotionEvent.ACTION_DOWN:
+                        isRightPressed = true;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        isRightPressed = false;
+                        break;
+                }
+                break;
+        }
+        return true;
+    }
+
+}
